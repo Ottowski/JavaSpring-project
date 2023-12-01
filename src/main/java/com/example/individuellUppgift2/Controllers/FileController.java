e5d9f450219ec8373f6abbc0d72696269871bcd5
Ottowski <otto.arvidsson94@gmail.com>
2023-12-01 14:04:38 +0100

    downloadfunctionDone

--- src/main/java/com/example/individuellUppgift2/Controllers/FileController.java
+++ src/main/java/com/example/individuellUppgift2/Controllers/FileController.java
@@ -3,10 +3,14 @@ package com.example.individuellUppgift2.Controllers;
 import com.example.individuellUppgift2.Service.FileService;
 import com.example.individuellUppgift2.Service.FolderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +22,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import com.example.individuellUppgift2.Service.FileService;
+import com.example.individuellUppgift2.Service.FolderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.core.io.Resource; // Alias for org.springframework.core.io.Resource
+
 
 @RestController
 @RequestMapping("/api/files")
@@ -45,33 +73,7 @@ public class FileController {
         }
 
         try {
-            // Get the username of the authenticated user
-            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-            String username;
-
-            // Check if the principal is an instance of UserDetails
-            if (principal instanceof UserDetails) {
-                UserDetails userDetails = (UserDetails) principal;
-                username = userDetails.getUsername();
-            } else if (principal instanceof String) {
-                // If not UserDetails, check if it's a String
-                username = (String) principal;
-            } else {
-                // Handle the case where principal is neither UserDetails nor String
-                throw new IllegalStateException("Unexpected principal type: " + principal.getClass());
-            }
-
-            // Debug statements to check authentication details
-            logger.info("Principal: {}", principal);
-            if (principal instanceof UserDetails) {
-                UserDetails userDetails = (UserDetails) principal;
-                logger.info("Username from UserDetails: {}", userDetails.getUsername());
-            } else if (principal instanceof String) {
-                logger.info("Username from String: {}", principal);
-            } else {
-                logger.warn("Unexpected principal type: {}", principal.getClass());
-            }
+            String username = getUsernameFromAuthentication();
 
             // Create the folder using folderService
             folderService.createFolder(username);
@@ -100,4 +102,46 @@ public class FileController {
             return ResponseEntity.status(500).body("File upload failed. Please try again.");
         }
     }
+
+    @GetMapping("/download")
+    public <SpringResource> ResponseEntity<Object> downloadFile(@RequestParam("filename") String filename) {
+        try {
+            String username = getUsernameFromAuthentication();
+
+            // Define the folder and file paths
+            String folderPath = UPLOAD_DIR + username + "/";
+            String filePath = folderPath + filename;
+
+            // Log file path
+            logger.info("File path: {}", filePath);
+
+            // Load the file as a resource
+            SpringResource resource = (SpringResource) new UrlResource(Paths.get(filePath).toUri());
+
+            // Set content type and attachment disposition
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", filename);
+
+            return ResponseEntity.ok().headers(headers).body(resource);
+        } catch (Exception e) {
+            // Handle file not found or other exceptions
+            logger.error("Error during file download: {}", e.getMessage(), e);
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
+    private String getUsernameFromAuthentication() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+            return userDetails.getUsername();
+        } else if (principal instanceof String) {
+            return (String) principal;
+        } else {
+            throw new IllegalStateException("Unexpected principal type: " + principal.getClass());
+        }
+    }
+
 }
